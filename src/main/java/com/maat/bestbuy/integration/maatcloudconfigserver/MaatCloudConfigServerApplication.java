a5f4045ab017dbf4152db6c49c94a0d2be3f1846
Chandrakanth Peravalli <Chandrakanth.Peravalli@bestbuy.com>
2018-03-30 16:21:20 -0500

    optimized application properties
    Signed-off-by: Chandrakanth Peravalli <Chandrakanth.Peravalli@bestbuy.com>

--- src/main/java/com/maat/bestbuy/integration/maatcloudconfigserver/MaatCloudConfigServerApplication.java
+++ src/main/java/com/maat/bestbuy/integration/maatcloudconfigserver/MaatCloudConfigServerApplication.java
@@ -1,9 +1,12 @@
 package com.maat.bestbuy.integration.maatcloudconfigserver;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.config.server.EnableConfigServer;
-
+@EnableEncryptableProperties
+@EnableConfigurationProperties
 @EnableConfigServer
 @SpringBootApplication
 public class MaatCloudConfigServerApplication {
