b0240d8b78f68fe177fd55e3585bbba9383d6997
Chandrakanth Peravalli <Chandrakanth.Peravalli@bestbuy.com>
2018-04-02 10:18:50 -0500

    Initial Commit
    Signed-off-by: Chandrakanth Peravalli <Chandrakanth.Peravalli@bestbuy.com>

--- src/main/java/com/spring/cloud/configServer/SpringCloudConfigServer.java
+++ src/main/java/com/spring/cloud/configServer/SpringCloudConfigServer.java
@@ -1,17 +1,18 @@
-package com.maat.bestbuy.integration.maatcloudconfigserver;
+package com.spring.cloud.configServer;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.config.server.EnableConfigServer;
+
 @EnableEncryptableProperties
 @EnableConfigurationProperties
 @EnableConfigServer
 @SpringBootApplication
-public class MaatCloudConfigServerApplication {
+public class SpringCloudConfigServer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MaatCloudConfigServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigServer.class, args);
+    }
 }
